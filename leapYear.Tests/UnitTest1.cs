cd202b9d4c02550722844f299a374fdb1418f4fc
Thor Lyster Lind <80474534+ThorLL@users.noreply.github.com>
2021-09-06 15:39:45 +0200

    Tested and implemented IsLeapYear

---
+++ leapYear.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace leapYear.Tests
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void IsLeapYearReturnsTrue()
+        {
+            Assert.True(Program.IsLeapYear(2000));
+            Assert.True(Program.IsLeapYear(4));
+            Assert.True(Program.IsLeapYear(-4));
+            Assert.True(Program.IsLeapYear(0));
+
+        }
+        [Fact]
+        public void IsLeapYearReturnsFalse()
+        {
+            Assert.False(Program.IsLeapYear(1800));
+            Assert.False(Program.IsLeapYear(3));
+            Assert.False(Program.IsLeapYear(-1700));
+        }
+    }
+}
---
+++ leapYear/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace leapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static bool IsLeapYear(int year){
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                return year % 400 == 0;
+            }else {
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+    }
+    
+
+}
