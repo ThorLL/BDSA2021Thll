ee0ab637ccae751502823768cb5f38ee9681dce5
ThorLL <80474534+ThorLL@users.noreply.github.com>
2021-09-09 06:51:08 -0700

    Feature/leap year (#3)
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear
    
    * Implemented error handling
    
    * Implemented error handling
    
    * Added documentaion
    
    * test from rider
    
    * Update BDSA2021Thll.sln
    
    * Update BDSA2021Thll.sln

---
+++ leapYear/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace leapYear
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a year to test whether or not it's a leap year, hit enter afterwards.");
+            while (true)
+            {
+                var input = Console.ReadLine();
+                try
+                {
+                    int year = int.Parse(input);     
+                    if(IsLeapYear(year))
+                    {
+                        Console.WriteLine("Yay");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nay");
+                        return;
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("'"+ input + "' is not an integer, e.g. 420");
+                }
+            }
+            
+        }
+        public static bool IsLeapYear(int year){
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                return year % 400 == 0;
+            }else {
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+    }
+    
+
+}
